6c434ee0f22be1eeddbaeb0822ad7fdb35e821d9
KelpGF <kelpgf@gmail.com>
2022-01-24 21:46:43 -0300

    login

--- src/modules/auth/pages/Login.vue
+++ src/modules/auth/pages/Login.vue
@@ -1,13 +1,79 @@
 <template>
-  <div>
-    <form @submit.prevent='submitForm()'>
-      <input type='text' v-model='formLogin.email'>
-      <input type='password' v-model='formLogin.password'>
-      <button type="submit">
-        Entrar
-      </button>
-    </form>
-  </div>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12 mt-16">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>
+                <v-icon>mdi-home-import-outline</v-icon> Bem vindo!
+              </v-toolbar-title>
+            </v-toolbar>
+            <v-card-text class="mt-2">
+              <v-form
+                v-model="valid"
+                lazy-validation
+                @submit.prevent='submitForm()'
+              >
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      prepend-icon="mdi-email"
+                      v-model="formLogin.email"
+                      label="Email"
+                      :rules="emailRules"
+                      outlined
+                      shaped
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      prepend-icon="mdi-lock"
+                      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="showPassword ? 'text' : 'password'"
+                      :rules="[v => !!v || 'Informe a senha']"
+                      label="Senha"
+                      class="input-group--focused"
+                      @click:append="showPassword = !showPassword"
+                      v-model='formLogin.password'
+                      outlined
+                      shaped
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-expand-transition>
+                  <v-row v-show="showMessageFail">
+                    <v-col>
+                      <v-alert
+                        dense
+                        border="left"
+                        type="error"
+                      >
+                        {{ messageFail }}
+                      </v-alert>
+                    </v-col>
+                  </v-row>
+                </v-expand-transition>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    :disabled="!valid"
+                    color="primary"
+                    class="mb-4 mt-0 mr-5"
+                    @click="submitForm()"
+                  >
+                    <v-icon>mdi-login</v-icon> Entrar
+                  </v-btn>
+                </v-row>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
@@ -15,11 +81,28 @@ import { mapActions } from 'vuex'
 
 export default {
   data: () => ({
+    valid: true,
+    showPassword: false,
     formLogin: {
       email: 'k@g.c',
       password: '123'
-    }
+    },
+    emailRules: [
+      v => !!v || 'Informe o email',
+      v => /.+@.+\..+/.test(v) || 'Email inválido'
+    ],
+    messageFail: '',
+    showMessageFail: false
   }),
+
+  watch: {
+    formLogin: {
+      deep: true,
+      handler: function () {
+        this.showMessageFail = false
+      }
+    }
+  },
 
   methods: {
     ...mapActions('auth', ['ActionLogin']),
@@ -30,7 +113,8 @@ export default {
 
         this.$router.push({ path: '/' })
       } catch (error) {
-        alert(error.data ? error.data.message : 'Não foi possível realizar o login!')
+        this.messageFail = error.data ? error.data.message : 'Não foi possível realizar o login!'
+        this.showMessageFail = true
       }
     }
   }
